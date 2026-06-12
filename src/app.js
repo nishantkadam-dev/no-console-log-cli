102d00cf97a86a9bc2fd88284d643fd50368853f
nishantkadam-dev <nishant.k@incubyte.co>
2026-06-12 16:55:42 +0530

    test: intentional console.log to verify CI failure summary

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 export function doSomething(input) {
-  // console.log("debug info");
+  console.log("debug info");
   console.warn("this is fine");
-  // console.log(.input);
+  console.log(input);
 }
